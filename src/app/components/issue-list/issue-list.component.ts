1852bcb8cb5d386cf538fa7b56f7899d4d082d4b
rouzbeh.majlessi@avanade.com <rouzbeh.majlessi@avanade.com>
2019-12-27 22:29:48 -0800

    add issue-list component and delete and load mehtods

--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BugService } from './../../shared/bug.service';
+
 
 @Component({
   selector: 'app-issue-list',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueListComponent implements OnInit {
 
-  constructor() { }
+  IssuesList: any = [];
+
+  constructor(
+    public bugService: BugService
+  ) { }
 
   ngOnInit() {
   }
+
+  loadEmployees() {
+    return this.bugService.getIssues().subscribe((data: {}) => {
+      this.IssuesList = data;
+    });
+  }
+
+  deleteIssue(data) {
+    var index = index = this.IssuesList.map(x => {
+      return x.issue_name;
+    }).indexOf(data.issue_name);
+    return this.bugService.deleteBug(data.id).subscribe(res => {
+      this.IssuesList.splice(index, 1);
+      console.log('issue deleted!');
+    });
+  }
+
+
 
 }
