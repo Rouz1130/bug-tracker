e35e1dac4859a5cc71465de4a5bb0e910e7bf5b4
rouzbeh.majlessi@avanade.com <rouzbeh.majlessi@avanade.com>
2019-12-27 19:46:26 -0800

    Add post method and errorhandler methods in bug service

--- src/app/shared/bug.service.ts
+++ src/app/shared/bug.service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@angular/core';
-
-import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Bug } from './bug';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-
 @Injectable({
   providedIn: 'root'
 })
 export class BugService {
-
   baseurl = 'http://localhost:3000';
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   // Http Headers
   httpOptions = {
@@ -22,5 +17,27 @@ export class BugService {
       'Content-Type': 'application/json'
     })
   };
+  // Post
+  createBug(data): Observable<Bug> {
+    return this.http
+      .post<Bug>(
+        this.baseurl + '/bugtracking/',
+        JSON.stringify(data),
+        this.httpOptions
+      )
+      .pipe(retry(1), catchError(this.errorHandl));
+  }
 
+  errorHandl(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
